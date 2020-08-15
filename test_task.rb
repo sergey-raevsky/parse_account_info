a1f98f5666f93fd588267d83ab186be38dde0eb2
Serghei <serghei.raevschii@inbox.ru>
2020-08-15 13:52:20 +0300

    Update test_task.rb
    draft

--- test_task.rb
+++ test_task.rb
@@ -29,6 +29,7 @@ browser.a(id: "lnkContracts").click
 # Кликаем на тумблер для вывода всех счетов
 browser.div(class: ["point", "text-center"]).click
 
+
 sleep 4
 # Парсинг страницы с nokogiri
 doc = Nokogiri::HTML.parse(browser.html)
@@ -43,11 +44,32 @@ account_info.each do |row|
 # Выводим данные в массив  
    accounts.push(
     account: td[1].text,
-    currency: td[2].text
+    currency: td[2].text.delete("Счёт "),
+    balance: td[4].text.chop.delete(" ")
   )
 end
-# Переводим в формат json 
-tableJson = accounts.to_json
 
-p tableJson
+
+
+# Переводим в формат json 
+#tableJson = accounts.to_json
+
+#p tableJson
+
+first_row = browser.table(id: "contracts-list")[4].td(class: "span").click
+
+sleep 4
+doc = Nokogiri::HTML.parse(browser.html)
+
+account_info = doc.css("#cards-list").css("tr.blockBody")
+p account_info[0].css('td')[4].text
+
+accounts.push(
+   nature: account_info[0].css('td')[4].text account_info[1].css('td')[4].text
+)
+ 
+   sleep 4
+
+    tableJson = accounts.to_json
+    p tableJson
 
